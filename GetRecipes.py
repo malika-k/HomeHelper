bd3b259b770e3059c3cd9382812b1d6af004be78
danikc8 <danikc8@gmail.com>
2019-03-03 08:34:07 -0500

    Merge branch 'Dani' of /home/danikc8/IdeaProjects/HomeHelper with conflicts.

--- GetRecipes.py
+++ GetRecipes.py
@@ -1,4 +1,13 @@
 import json
 import urllib.request
+import random
+
 
 def getRecipes():
+    url = "https://api.wegmans.io/meals/recipes?api-version=2018-10-18&subscription-key=d1cef5a8c3434b71a0ddfc4ee471c7cc"
+    response = urllib.request.urlopen(url)
+    contentString = response.read().decode()
+    content = json.loads(contentString)
+    content2 = content["recipes"]
+    randomRecipe = random.choice(content2)
+    return randomRecipe["name"]
